ff265d19a970562a9973348b0d653103169d3e20
Nguyen Thanh Bien <bien@bienlab.com>
2019-03-20 16:20:09 +0700

    Update iboard-cut-URI.tcl

--- iboard-cut-URI.tcl
+++ iboard-cut-URI.tcl
@@ -1,6 +1,6 @@
 when HTTP_REQUEST {
   switch -glob [string tolower [HTTP::path]]  {
-      "/gateway*" {
+      "/gateway/*" {
         HTTP::uri [substr $uri 8]
         pool pool_gateway
         log local0.info "Sending to pool gateway [HTTP::uri] [IP::client_addr] [IP::local_addr]"
