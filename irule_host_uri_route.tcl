901da68822d66e95d9b7d908b60a76200eab898c
Bien <bien@bienlab.com>
2020-07-01 21:00:06 +0700

    update irule_host_uri_route

--- irule_host_uri_route.tcl
+++ irule_host_uri_route.tcl
@@ -1,26 +1,10 @@
 when HTTP_REQUEST {
 switch -glob [HTTP::header Host] {
-  "gateway.congdulieuyte.vn" {
-  	pool HIS_Portal1
+  "abc.vn" {
+    pool abcpool
   }
-  "congdulieuyte.vn" {
-  	if {[HTTP::uri] starts_with "/hPortal/services"} {
-  		pool HIS_Portal2
-  	} elseif {[HTTP::uri] starts_with "/dreport"} {
-  		pool HIS_Portal3
-  		} else {
-  				pool HIS_Portal4
-  			}
-  }
-  "hanam.congdulieuyte.vn" {
-  	pool HIS_Hanam
-   
-  }
-  "hssk.congdulieuyte.vn" {
-    pool HIS_Portal6
-  }
-  "quanlyduoc.congdulieuyte.vn" {
-    pool HIS_Portal7
+  "xyz.vn" {
+    pool xyzpool
   }
 }
 }
